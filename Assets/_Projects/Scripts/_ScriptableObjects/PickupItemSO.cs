3f5947389a5b3c02a81b5f1513a4eb56e7f87d3d
MoZ0311 <mozmegane0311@gmail.com>
2026-02-20 11:10:28 +0900

    ステータス成長率の定義を移動

--- Assets/_Projects/Scripts/_ScriptableObjects/PickupItemSO.cs
+++ Assets/_Projects/Scripts/_ScriptableObjects/PickupItemSO.cs
@@ -5,5 +5,6 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "PickupItemSO", menuName = "Scriptable Objects/PickupItemSO")]
 public class PickupItemSO : ScriptableObject
 {
+    // ステージ上での生成アイテムをリスト型で格納
     [field:SerializeField] public List<NetworkObject> PickupItemList { get; private set; } = new();
 }
--- Assets/_Projects/Scripts/_ScriptableObjects/PlayerParameterSO.cs
+++ Assets/_Projects/Scripts/_ScriptableObjects/PlayerParameterSO.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 public class PlayerParameterSO : ScriptableObject
 {
     [Header("Player Settings")]
-    [field:SerializeField] public float Health { get; private set; }        // 体力
-    [field:SerializeField] public float MoveSpeed { get; private set; }     // 移動速度
+    [SerializeField] float health;               // 体力
+    [SerializeField] float moveSpeed;            // 移動速度
+
+    public float Health => health;
+    public float MoveSpeed => moveSpeed;
 
     [Header("Bullet Settings")]
-    [field:SerializeField] public float AttackPower { get; private set; }   // 攻撃力
-    [field:SerializeField] public float BulletSpeed { get; private set; }   // 弾速
-    [field:SerializeField] public float FireRate { get; private set; }      // 連射速度(発/s)
-    [field:SerializeField] public bool CanRapidFire { get; private set; }   // 自動で連射できるか
-    [field:SerializeField] public float ReloadTime { get; private set; }    // リロード時間
+    [SerializeField] float attackPower;           // 攻撃力
+    [SerializeField] float bulletSpeed;           // 弾速
+    [SerializeField] float fireRate;              // 連射速度(発/s)
+    [SerializeField] bool canRapidFire;           // 自動で連射できるか
+    [SerializeField] float reloadTime;            // リロード時間
+
+    public float AttackPower => attackPower;
+    public float BulletSpeed => bulletSpeed;
+    public float FireRate => fireRate;
+    public bool CanRapidFire => canRapidFire;
+    public float ReloadTime => reloadTime;
+
+    [Header("GrowthRate Settings")]
+    [SerializeField] float scaleGrowthRate;       // 大きさの成長率
+    [SerializeField] float healthGrowthRate;      // 体力の成長率
+    [SerializeField] float moveSpeedGrowthRate;   // 移動速度の成長率
+    [SerializeField] float attackPowerGrowthRate; // 攻撃力の成長率
+    [SerializeField] float bulletSpeedGrowthRate; // 弾速の成長率
+    [SerializeField] float fireRateGrowthRate;    // 連射速度の成長率
+    [SerializeField] float reloadTimeGrowthRate;  // リロード時間の成長率
+
+    public float ScaleGrowthRate => scaleGrowthRate;
+    public float HealthGrowthRate => healthGrowthRate;
+    public float MoveSpeedGrowthRate => moveSpeedGrowthRate;
+    public float AttackPowerGrowthRate => attackPowerGrowthRate;
+    public float BulletSpeedGrowthRate => bulletSpeedGrowthRate;
+    public float FireRateGrowthRate => fireRateGrowthRate;
+    public float ReloadTimeGrowthRate => reloadTimeGrowthRate;
 }
